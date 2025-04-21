ad1ba817480ddf9927825ca386f292c35e79ec5c
pianstore <stoksipu@gmail.com>
2025-04-21 18:57:11 +0800

    Update ohp.sh

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
- wget -O /usr/local/bin/ohpserver "http://myrid.my.id/os/sshws/ohpserver"
+ wget -O /usr/local/bin/ohpserver "https://raw.githubusercontent.com/pianstore/sshws/ohpserver"
  chmod +x /usr/local/bin/ohpserver
 # Installing Service
 # SSH OHP Port 8181
